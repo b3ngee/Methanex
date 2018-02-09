4cd8d06959fd4e731140203d15ad48117cb4501b
Lansi Chu <chulansi@gmail.com>
2018-02-08 19:48:32 -0800

    Fixed merge conflicts

--- client/app/components/App.js
+++ client/app/components/App.js
@@ -2,21 +2,12 @@ import React from 'react';
 // import { footer } from '../styles/footer.scss';
 import Routes from '../routes';
 import Navbar from './Navbar';
+// import { Link } from 'react-router-dom';
 
 const App = () =>
     <div>
         <Navbar/>
         { Routes }
-const App = () =>
-    <div>
-        <h2>Methanex</h2>
-        { Routes }
-        <footer className={ footer }>
-            <Link to="/portfolio">My portfolio</Link>
-            <Link to="/projects">My projects</Link>
-            <Link to="/resources">My resources</Link>
-        </footer>
-
     </div>;
 
 export default App;
