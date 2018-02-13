36a9d47921cc36685ea0e4b506e10c29e550b7b1
b3ngee <bg.92@live.com>
2018-02-13 10:53:13 -0800

    Added Display componenet that fits beside navbar

--- client/app/components/App.js
+++ client/app/components/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-// import { footer } from '../styles/footer.scss';
-import Routes from '../routes';
 import Navbar from './Navbar';
-// import { Link } from 'react-router-dom';
+import Display from './Display';
 
 const App = () =>
     <div>
         <Navbar/>
-        { Routes }
+        <Display/>
     </div>;
 
 export default App;
