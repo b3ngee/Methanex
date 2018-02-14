8cdae50866205030eb3a8cb78c842fbcff7a6478
Ji Yoon Ok <jiyoonok@dhcp-206-12-197-9.ubcsecure.wireless.ubc.ca>
2018-02-14 11:44:52 -0800

    modified display css width, displaying mock data in resources

--- client/app/components/Display.js
+++ client/app/components/Display.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Routes from '../routes';
 import { display } from '../styles/display.scss';
+import Toolbar from './Toolbar';
 
 class Display extends Component {
     render() {
         return (
             <div className={ display }>
+                <Toolbar/>
                 { Routes }
             </div>
         );
--- client/app/components/Resource.js
+++ client/app/components/Resource.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import { resource } from '../styles/resource.scss';
+import Table from './Table.js';
+
+
+let tableData = {
+    columns: ['Resource Name', 'Resource Manager', 'Skills', 'Department'],
+    rows: [{
+        'Resource Name': 'Daniel Chen',
+        'Resource Manager': 'Jane Doe',
+        'Skills': 'Java, SQL',
+        'Department': 'IT'
+    }, {
+        'Resource Name': 'Lansi Chu',
+        'Resource Manager': 'Daniel Choi',
+        'Skills': 'Java, React',
+        'Department': 'IT'
+    }, {
+        'Resource Name': 'Harnoor Shoker',
+        'Resource Manager': 'Daniel Choi',
+        'Skills': 'Java, MySQL',
+        'Department': 'IT'
+    }, {
+        'Resource Name': 'Ben Gee',
+        'Resource Manager': 'Jane Doe',
+        'Skills': 'Java',
+        'Department': 'Business'
+    }, {
+        'Resource Name': 'Stella Fang',
+        'Resource Manager': 'Jane Doe',
+        'Skills': 'Java',
+        'Department': 'Business'
+    }]
+};
 
 const Resource = () =>
     <div className={ resource }>
         <h1>My Resource</h1>
+        <Table text="List of Resources" data={tableData}/>
     </div>;
 
 export default Resource;
