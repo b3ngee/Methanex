f61463cf1b08a10c4b1af6c657db851e2e8ddf4b
Colby Song <colbysong@yahoo.ca>
2018-02-20 16:52:37 -0800

    add field component, login stubs

--- client/app/components/Login.js
+++ client/app/components/Login.js
@@ -1,22 +1,64 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { login } from '../styles/login.scss';
+import TextFieldGroup from './TextFieldGroup';
 
-const Login = () =>
-    <div className={ login }>
-        <h1>Please log in...</h1>
-        <label htmlFor="user_name">
-            Username:
-            <input id="user_name" type="text"/>
-        </label>
-        <br/>
-        <br/>
-        <label htmlFor="pw">
-            Password:
-            <input id="pw" type="password"/>
-        </label>
-        <br/>
-        <br/>
-        <Link to="/">Login</Link>
-    </div>;
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            identifier: '',
+            password: '',
+            errors: {},
+            isLoading: false
+        };
+
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+    isValid() {
+
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    render() {
+        const { errors, identifier, password, isLoading } = this.state;
+
+        return (
+            <form onSubmit={this.onSubmit}>
+                <h1>Login</h1>
+
+                { errors.form && <div className="alert alert-danger">{errors.form}</div> }
+
+                <TextFieldGroup
+                    field="identifier"
+                    label="Username / Email"
+                    value={identifier}
+                    error={errors.identifier}
+                    onChange={this.onChange}
+                />
+
+                <TextFieldGroup
+                    field="password"
+                    label="Password"
+                    value={password}
+                    error={errors.password}
+                    onChange={this.onChange}
+                    type="password"
+                />
+
+                <div className="form-group">
+                    <button className="loginButton" disabled={isLoading}>Login</button>
+                </div>
+            </form>
+        );
+    }
+}
+
 export default Login;
