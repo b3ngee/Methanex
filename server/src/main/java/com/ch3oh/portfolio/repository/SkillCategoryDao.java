e579874a4cd163dbfde9269f36f8fd187fc027d0
dchen121 <dchen121@gmail.com>
2018-03-06 13:41:45 -0800

    Check for duplicate skill categories

--- server/src/main/java/com/ch3oh/portfolio/repository/SkillCategoryDao.java
+++ server/src/main/java/com/ch3oh/portfolio/repository/SkillCategoryDao.java
@@ -1,8 +1,20 @@
 package com.ch3oh.portfolio.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.ch3oh.portfolio.persistence.SkillCategory;
 
 public interface SkillCategoryDao extends CrudRepository<SkillCategory, Integer> {
+
+    /**
+     * Finds SkillCategory that matches specified skillCategoryName
+     *
+     * @param skillCategoryName
+     * @return A SkillCategory
+     * If no SkillCategory is found, return null.
+     */
+    @Query("SELECT s FROM SkillCategory s WHERE s.name = ?1")
+    SkillCategory findBySkillCategoryName(@Param("name") String skillCategoryName);
 }
