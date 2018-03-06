e502b08a4f373e0e61ebc47d854460b5774dca16
Colby Song <colbysong@yahoo.ca>
2018-03-05 20:32:15 -0800

    remove location hack and use react router, use image link instead of image path

--- client/app/components/Navbar.js
+++ client/app/components/Navbar.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router';
 import { navbar } from '../styles/navbar.scss';
 import { logout } from '../styles/logout.scss';
 import '../styles/global.scss';
 
 class Navbar extends Component {
     // TODO: conditionally render different links depending on roles retrieved from local storage
+    constructor(props) {
+        super(props);
+    }
+
     logout() {
         localStorage.clear();
-        location.reload();
+        this.props.history.push('/');
     }
 
     render() {
         return (
             <div className={ navbar }>
-                <img src={require('../images/methanexlogo.png')} />
+                <img src="https://i.imgur.com/gUCwlxg.png" />
                 {
                     localStorage.getItem('user_id') &&
                     <div>
@@ -23,7 +28,7 @@ class Navbar extends Component {
                         <Link to="/project">Projects</Link>
                         <Link to="/resource">Resources</Link>
                         <Link to="/setting">Settings</Link>
-                        <div className={ logout } onClick={this.logout}> Log Out </div>
+                        <div className={ logout } onClick={this.logout.bind(this)}> Log Out </div>
                     </div>
                 }
             </div>
@@ -31,5 +36,9 @@ class Navbar extends Component {
     }
 }
 
-export default Navbar;
+Navbar.propTypes = {
+    history: React.PropTypes.object
+};
+
+export default withRouter(Navbar);
 
