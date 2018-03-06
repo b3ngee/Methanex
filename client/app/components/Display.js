bea84c6c0a5a84fa14a02de9b393d638c8285a2f
Colby Song <colbysong@yahoo.ca>
2018-03-05 22:12:47 -0800

    remove toolbar

--- client/app/components/Display.js
+++ client/app/components/Display.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import Routes from '../routes';
 import { display } from '../styles/display.scss';
-import Toolbar from './Toolbar';
 
 class Display extends Component {
     render() {
         return (
             <div className={ display }>
-                <Toolbar/>
                 { Routes }
             </div>
         );
