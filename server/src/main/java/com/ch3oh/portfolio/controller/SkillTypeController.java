934e7b2db5171d0b6189f31bbbc15c4b2a6ee8f2
dchen121 <dchen121@gmail.com>
2018-03-06 13:46:27 -0800

    Add skill type CRUD

---
+++ server/src/main/java/com/ch3oh/portfolio/controller/SkillTypeController.java
@@ -0,0 +1,55 @@
+package com.ch3oh.portfolio.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ch3oh.portfolio.persistence.SkillType;
+import com.ch3oh.portfolio.service.SkillTypeServiceImpl;
+
+@RestController
+@RequestMapping(path = "/skill-types")
+public class SkillTypeController {
+
+    @Autowired
+    private SkillTypeServiceImpl skillTypeService;
+
+    @GetMapping(value = "/{id}")
+    @ResponseBody
+    public SkillType getSkillType(@PathVariable("id") String id) {
+        return skillTypeService.getSkillType(id);
+    }
+
+    @GetMapping
+    @ResponseBody
+    public Iterable<SkillType> getSkillTypes() {
+        return skillTypeService.getSkillTypes();
+    }
+
+    @PostMapping
+    @ResponseBody
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public SkillType createSkillType(@RequestBody SkillType skillType) {
+        return skillTypeService.createSkillType(skillType);
+    }
+
+    @PutMapping(value = "/{id}")
+    @ResponseBody
+    public SkillType updateSkillType(@PathVariable("id") String id, @RequestBody SkillType toUpdate) {
+        return skillTypeService.updateSkillType(id, toUpdate);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteSkillType(@PathVariable("id") String id) {
+        skillTypeService.deleteSkillType(id);
+    }
+}
