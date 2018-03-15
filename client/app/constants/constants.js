236823847188e3df702bf5a8126b34c304dc7217
Colby Song <colbysong@yahoo.ca>
2018-03-14 20:33:50 -0700

    login updated, conditional rendering of nav bar done

---
+++ client/app/constants/constants.js
@@ -0,0 +1,10 @@
+// API endpoints
+export const prodAPIEndpoint = 'https://methanex-portfolio-management.herokuapp.com';
+export const apiaryAPIEndpoint = 'https://private-2a709-methanex.apiary-mock.com';
+
+// User roles
+export const RESOURCE_MANAGER = 'RESOURCE_MANAGER';
+export const SUPER_ADMIN = 'SUPER_ADMIN';
+export const PORTFOLIO_MANAGER = 'PORTFOLIO_MANAGER';
+export const RESOURCE = 'RESOURCE';
+export const PROJECT_MANAGER = 'PROJECT_MANAGER';
