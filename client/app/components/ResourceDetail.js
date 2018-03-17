ca28b4e28b8b5e7754a0244e959f3575071b7b5b
Lansi Chu <chulansi@gmail.com>
2018-03-17 14:33:04 -0700

    display resource skills in resource detail with skills names

--- client/app/components/ResourceDetail.js
+++ client/app/components/ResourceDetail.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Table from './Table.js';
 import { resource } from '../styles/resource.scss';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Button from './Button.js';
 
 class ResourceDetail extends React.Component {
 
@@ -9,6 +11,8 @@ class ResourceDetail extends React.Component {
         super(props);
         this.state = {
             rows: [],
+            skillsRows: [],
+            skillsNames: {}
         };
 
         this.getDetails = this.getDetails.bind(this);
@@ -16,9 +20,9 @@ class ResourceDetail extends React.Component {
 
     componentDidMount() {
         this.getDetails();
+        this.getSkillsNames();
     }
 
-    // TODO: need to fix up the apiary call
     getDetails() {
         console.log('in getdetails');
         console.log(this.props.match.params.resource_id);
@@ -33,16 +37,49 @@ class ResourceDetail extends React.Component {
             }
             console.log(data);
             this.setState({rows: rows});
+            }).catch( () => {
+        });
+    }
+
+    getUserSkills() {
+        axios.get('https://methanex-portfolio-management.herokuapp.com/user-skills?userId=' + this.props.match.params.resource_id).then(response => {
+            const skillsRows = [];
+            const data = response.data;
+
+            for (let i = 0; i < response.data.length; i++) {
+                skillsRows.push({ 'Skill': this.state.skillsNames[data[i].skillTypeId], 'Competency': data[i].competency });
+            }
+            this.setState({skillsRows: skillsRows});
+        }).catch( () => {
+        });
+    }
+
+    getSkillsNames() {
+        axios.get('https://methanex-portfolio-management.herokuapp.com/skill-types').then(response => {
+            const data = {};
+            for (let i = 0; i < response.data.length; i++) {
+                data[response.data[i].id] = response.data[i].name;
+            }
+            this.setState({ skillsNames: data });
+            this.getUserSkills();
         }).catch( () => {
         });
     }
 
     render() {
-        let columns = ['key', 'value'];
+        let columns = ['Header', 'Value'];
+        let skillsColumns = ['Skill', 'Competency'];
+        const data = this.state.rows;
         return (
             <div className={ resource }>
                 <h1>Resource Details</h1>
                 <Table text="Resource Details" columns={columns} rows={this.state.rows}/>
+                <span>
+                    <Link to={{pathname: '/resource/edit', state: {data}}}>
+                        <Button label="Edit"/>
+                    </Link>
+                </span>
+                <Table text="Resource Skills" columns={skillsColumns} rows={this.state.skillsRows} />
             </div>
         );
     }
