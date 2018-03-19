5eabc21b40cb83bea589fac7d47280674f18f3fc
Colby Song <colbysong@yahoo.ca>
2018-03-19 14:17:56 -0700

    fix no role user navbar rendering

--- client/app/components/Navbar.js
+++ client/app/components/Navbar.js
@@ -11,11 +11,6 @@ import {
 } from '../constants/constants';
 import '../styles/global.scss';
 
-let isAdmin = false;
-let isPortfolioManager = false;
-let isResourceManager = false;
-let isProjectManager = false;
-
 class Navbar extends Component {
     constructor(props) {
         super(props);
@@ -27,8 +22,13 @@ class Navbar extends Component {
     }
 
     render() {
+        let isAdmin = false;
+        let isPortfolioManager = false;
+        let isResourceManager = false;
+        let isProjectManager = false;
         const roles = localStorage.getItem('roles');
         if (roles) {
+            console.log(roles);
             isAdmin = roles.includes(SUPER_ADMIN);
             isPortfolioManager = roles.includes(PORTFOLIO_MANAGER);
             isResourceManager = roles.includes(RESOURCE_MANAGER);
