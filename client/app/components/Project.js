d8cd81ffee1249422f4c76bb7f3ac3e32466e470
HGZHGZH <HGZHGZH@users.noreply.github.com>
2018-03-24 09:56:20 -0700

    resource can see what projects they are working on

--- client/app/components/Project.js
+++ client/app/components/Project.js
@@ -4,8 +4,9 @@ import Table from './Table';
 import Button from './Button';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
-import { SUPER_ADMIN, prodAPIEndpoint } from '../constants/constants';
+import { RESOURCE, SUPER_ADMIN, prodAPIEndpoint } from '../constants/constants';
 import { sanitizeProjectStatus, sanitizeRagStatus, sanitizeBudget } from '../utils/sanitizer';
+import PopupBox from './PopupBox';
 
 class Project extends React.Component {
     constructor(props) {
@@ -13,18 +14,45 @@ class Project extends React.Component {
         this.state = {
             numProject: 0,
             selectedProjectID: 0,
-            rows: []
+            rows: [],
+            userId: localStorage.getItem('user_id'),
+            roles: localStorage.getItem('roles'),
+            assignedProjectsRows: [],
+            errorModalOpen: false
         };
         this.getProjects = this.getProjects.bind(this);
+        this.getRowsForAssignedProjectsTable = this.getRowsForAssignedProjectsTable.bind(this);
+        this.onCloseError = this.onCloseError.bind(this);
     }
 
     componentDidMount() {
-        this.getProjects();
+        if(this.state.roles.includes(RESOURCE)) {
+            this.getRowsForAssignedProjectsTable();
+        } else {
+            this.getProjects();
+        }
+    }
+
+    getRowsForAssignedProjectsTable() {
+        const tableData = [];
+        axios.get(prodAPIEndpoint + '/project-resources?resourceId=' + this.state.userId, {headers: {Pragma: 'no-cache'}}).then(response => {
+            for (let i = 0; i < response.data.length; i++) {
+                axios.get(prodAPIEndpoint + '/projects/' + response.data[i].projectId, {headers: {Pragma: 'no-cache'}}).then(projResponse => {
+                    axios.get(prodAPIEndpoint + '/users/' + projResponse.data.managerId, {headers: {Pragma: 'no-cache'}}).then(userResponse => {
+                        tableData.push({
+                            'Projects': projResponse.data.name,
+                            'Project Manager Names': userResponse.data.firstName + ' ' + userResponse.data.lastName,
+                            'Hours': response.data[i].assignedHours
+                        });
+                        this.setState({ assignedProjectsRows: tableData });
+                    }).catch( (error) => { this.setState({ errorMessage: 'Error: ' + error.response.data.message, errorModalOpen: true }); });
+                }).catch( (error) => { this.setState({ errorMessage: 'Error: ' + error.response.data.message, errorModalOpen: true }); });
+            }
+        }).catch( (error) => { this.setState({ errorMessage: 'Error: ' + error.response.data.message, errorModalOpen: true }); });
     }
 
     getProjects() {
-        const roles = localStorage.getItem('roles');
-        const isAdmin = roles.includes(SUPER_ADMIN);
+        const isAdmin = this.state.roles.includes(SUPER_ADMIN);
         let query;
         if (isAdmin) {
             query = '/projects';
@@ -44,16 +72,38 @@ class Project extends React.Component {
                     'Budget': sanitizeBudget(this.state.projects[i].budget) });
             }
             this.setState({ rows: tableData});
-        }).catch( () => {
-        });
+        }).catch( (error) => { this.setState({ errorMessage: 'Error: ' + error.response.data.message, errorModalOpen: true }); });
+    }
+
+    onCloseError() {
+        this.setState({ errorModalOpen: false });
     }
 
     render() {
         let columns = ['ID', 'Project Name', 'Project Status', 'Status', 'Budget'];
-        const rows = this.state.rows;
+        let assignedProjectsColumns = ['Projects', 'Project Manager Names', 'Hours'];
+        const {rows, errorModalOpen, errorMessage} = this.state;
+        if (this.state.roles.includes(RESOURCE)) {
+            return (
+                <div className={ project }>
+                    <h3>Assigned Projects</h3>
+                    <PopupBox
+                        label={errorMessage}
+                        isOpen={errorModalOpen}
+                        onClose={this.onCloseError}
+                    />
+                    <Table text="List of Assigned Projects" columns={assignedProjectsColumns} rows={this.state.assignedProjectsRows}/>
+                </div>
+            );
+        }
         return(
             <div className={ project }>
                 <h1>My Projects</h1>
+                <PopupBox
+                    label={errorMessage}
+                    isOpen={errorModalOpen}
+                    onClose={this.onCloseError}
+                />
                 <h3>Number of projects: {this.state.rows.length}</h3>
                 <Table text="List of Projects" columns={columns} rows={this.state.rows}/>
                 <span>
